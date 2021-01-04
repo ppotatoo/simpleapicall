ef121883dadbbe0253035c9056f39d2f95da2902
ppotatoo <rkawas731@protonmail.com>
2021-01-03 21:08:27 -0500

    changed name

---
+++ apicall.py
@@ -0,0 +1,10 @@
+import json
+import requests
+
+with open("data.json", "w") as f:
+    data = requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
+    json.dump(data, f, indent=4)
+        
+
+
+
