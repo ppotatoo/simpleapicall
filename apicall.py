528f3cd3dc256a9430647e16424142a08e82dbc1
ppotatoo <rkawas731@protonmail.com>
2021-01-04 18:21:31 -0500

    Update to only include SCORESABER

--- apicall.py
+++ apicall.py
@@ -1,37 +1,32 @@
-import json
+import os
 import requests
 from pprint import pprint
 
-def choice():
-	c = input('Would you like to use ScoreSaber or BeatSavior. Please type carefully. \n')
-	if c == 'ScoreSaber':
-		scoresaber()
-	if c == 'BeatSavior':
-		beatsavior()
-
-def beatsavior():
-	uid = int(input('Insert your ScoreSaber ID: \n'))
-
-	with open("data.json", "w") as f:
-		data = requests.get(f'https://www.beatsavior.io/api/livescores/player/{uid}').json()
-		json.dump(data, f, indent=4)
-
-	with open("data.json", "r") as f:
-		data = json.load(f)
-
-	for data in data[0:5]:
-		print(f"Name: {data['songName']} Difficulty: {data['songDifficulty']}")
+def wipe():
+    os.system("cls")
 
 def scoresaber():
-	uid = (input('Insert your ScoreSaber ID: \n'))
+    uid = (input('Insert your ScoreSaber ID: \n'))
+    wipe()
+    data = requests.get(f'https://new.scoresaber.com/api/player/{uid}/full').json()
+    print("\nPlayer Info:")
+    print(f"    Player ID: {data['playerInfo']['playerId']}")
+    print(f"    Player Name: {data['playerInfo']['playerName']}")
+    print(f"    Avatar: https://new.scoresaber.com{data['playerInfo']['avatar']}")
+    print(f"    Country: {data['playerInfo']['country']}")
+    print(f"    Rank: {data['playerInfo']['rank']}")
+    print(f"    Country Rank: {data['playerInfo']['countryRank']}")
+    print(f"    Performance Points: {data['playerInfo']['pp']}")
+    print(f"    Role: {data['playerInfo']['role']}")
+    print("\nScore Stats:")
+    print(f"    Total Score: {data['scoreStats']['totalScore']}")
+    print(f"    Total Ranked Score: {data['scoreStats']['totalRankedScore']}")
+    print(f"    Average Ranked Accuracy: {data['scoreStats']['averageRankedAccuracy']}")
+    print(f"    Total Play Count: {data['scoreStats']['totalPlayCount']}")
+    print(f"    Ranked Play Count: {data['scoreStats']['rankedPlayCount']}\n")
+    scoresaber()
+scoresaber()
 
-	with open("data.json", "w") as f:
-		data = requests.get(f'https://new.scoresaber.com/api/player/{uid}/full').json()
-		json.dump(data, f, indent=4)
+#{data['playerInfo']
+#print(f"")
 
-	with open("data.json", "r") as f:
-		data = json.load(f)
-	
-	pprint(data)
-
-choice()
