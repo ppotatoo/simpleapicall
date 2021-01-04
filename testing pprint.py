567fa25e568007c5e950d89dc61c65fb8d41387e
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-01-03 21:07:10 -0500

    Update testing pprint.py
    remove junk

--- testing pprint.py
+++ testing pprint.py
@@ -1,15 +1,10 @@
 import json
 import requests
-from pprint import pprint
-
-with open("test.json", "r") as f:
-        data = json.load(f)
 
 with open("data.json", "w") as f:
     data = requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
     json.dump(data, f, indent=4)
+        
 
-data2 = requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
 
-pprint(data2)
 
