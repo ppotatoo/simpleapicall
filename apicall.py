4c015fb22fead46d01cbb59032b4343b334f9631
ppotatoo <rkawas731@protonmail.com>
2021-01-04 18:27:05 +0000

    changed stuff to f strings. ScoreSaber profile checking at the very least works.

--- apicall.py
+++ apicall.py
@@ -1,35 +1,36 @@
 import json
 import requests
 
-async def choice():
-	c = input('Would you like to use ScoreSaber or BeatSavior. Please type carefully.')
+def choice():
+	c = input('Would you like to use ScoreSaber or BeatSavior. Please type carefully. \n')
 	if c == 'ScoreSaber':
 		scoresaber()
 	if c == 'BeatSavior':
 		beatsavior()
 
-async def beatsavior():
+def beatsavior():
 	uid = int(input('Insert your ScoreSaber ID: \n'))
 
 	with open("data.json", "w") as f:
-    	data = requests.get(f'https://www.beatsavior.io/api/livescores/player/{uid}').json()
-    	json.dump(data, f, indent=4)
+		data = requests.get(f'https://www.beatsavior.io/api/livescores/player/{uid}').json()
+		json.dump(data, f, indent=4)
 
 	with open("data.json", "r") as f:
-        data = json.load(f)
-
+		data = json.load(f)
 
 	for data in data[0:5]:
-		print('Name: \''+data['songName']+'\' Difficulty: \''+data['songDifficulty']+'\'')
+		print(f"Name: {data['songName']} Difficulty: {data['songDifficulty']}")
 
-async def scoresaber():
-	uid = int(input('Insert your ScoreSaber ID: \n'))
+def scoresaber():
+	uid = (input('Insert your ScoreSaber ID: \n'))
 
 	with open("data.json", "w") as f:
-    	data = requests.get(f'https://new.scoresaber.com/api/player/{uid}/full').json()
-    	json.dump(data, f, indent=4)
+		data = requests.get(f'https://new.scoresaber.com/api/player/{uid}/full').json()
+		json.dump(data, f, indent=4)
 
 	with open("data.json", "r") as f:
-        data = json.load(f)
+		data = json.load(f)
 	
-	print('Name: \''+data['songName']+'\' Difficulty: \''+data['songDifficulty']+'\'')
+	print(f"Name: {data['playerInfo']['playerName']} Rank: {+data['playerInfo']['rank']}")
+
+choice()
