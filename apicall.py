76157dc06443df41c702e318543dc23bd9777d6b
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-01-03 21:09:36 -0500

    Update apicall.py
    another way

--- apicall.py
+++ apicall.py
@@ -4,6 +4,8 @@ import requests
 with open("data.json", "w") as f:
     data = requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
     json.dump(data, f, indent=4)
+    
+variable = requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
         
 
 
