d441c5634df755594b7528a87b7be0a8a2e883ea
ppotatoo <rkawas731@protonmail.com>
2021-01-04 16:15:10 +0000

    updated, now will show song difficulties and names

--- apicall.py
+++ apicall.py
@@ -2,7 +2,14 @@ import json
 import requests
 
 with open("data.json", "w") as f:
-    data = requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
+    data = requests.get('https://www.beatsavior.io/api/livescores/player/3032736043471367').json()
     json.dump(data, f, indent=4)
     
 variable = requests.get('https://jsonplaceholder.typicode.com/todos/1').json()
+
+with open("data.json", "r") as f:
+        data = json.load(f)
+
+
+for data in data[0:5]:
+	print('Name: \''+data['songName']+'\' Difficulty: \''+data['songDifficulty']+'\'')
